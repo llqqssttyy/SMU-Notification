a1bd2ea22870bc642a5da7286482ed58aa757758
Kim Da Eun <midekuna@gmail.com>
2023-09-15 00:54:13 +0900

    공식 사이트 업데이트로 Base URL 변경

--- index.js
+++ index.js
@@ -1,64 +1,69 @@
-import * as cheerio from 'cheerio';
-import axios from 'axios';
-import { makeHTML } from './utility/makeHTML.js';
-import { date, formattedDate, month } from './utility/date.js';
-import { sendMail } from './utility/sendMail.js';
+import * as cheerio from "cheerio";
+import axios from "axios";
+import { makeHTML } from "./utility/makeHTML.js";
+import { date, formattedDate, month } from "./utility/date.js";
+import { sendMail } from "./utility/sendMail.js";
 
 // 공지사항 페이지로 이동할 url을 생성하기 위해 base url과 query를 분리.
-const SMUOfficialBaseURL = `https://www.smu.ac.kr/lounge/notice/notice.do`;
+const SMUOfficialBaseURL = `https://www.smu.ac.kr/kor/life/notice.do`;
 const SMUOfficialQuery = `?srcCampus=smu&srStartDt=${formattedDate}&srEndDt=${formattedDate}&mode=list&srCategoryId1=&srSearchKey=&srSearchVal=`;
 
 // selectors
 const listSelector = "#ko > div.board-name-thumb.board-wrap > ul";
-const categorySelector = "dl > dt > table > tbody > tr > td:nth-child(2) > a > span.cate";
+const categorySelector =
+    "dl > dt > table > tbody > tr > td:nth-child(2) > a > span.cate";
 const titleSelector = "dl > dt > table > tbody > tr > td:nth-child(3) > a";
 const viewsSelector = "dl > dd > ul > li.board-thumb-content-views";
 
-axios.get(`${SMUOfficialBaseURL}${SMUOfficialQuery}`)
-.then((res) => {
-    if(res.status === 200) {
-        const $ = cheerio.load(res.data);   // full html doc
-        const $noticeList = $(listSelector).children('li');
-        
-        let todaysNoticeList = [];
+axios
+    .get(`${SMUOfficialBaseURL}${SMUOfficialQuery}`)
+    .then((res) => {
+        if (res.status === 200) {
+            const $ = cheerio.load(res.data); // full html doc
+            const $noticeList = $(listSelector).children("li");
 
-        $noticeList.each(function (idx, notice) {
-            const category = $(notice).find(categorySelector).text();
-            const title = $(notice).find(titleSelector).text();
-            const views = $(notice).find(viewsSelector).text();
-            const url = $(notice).find(titleSelector).attr('href');
-            
-            todaysNoticeList[idx] = {
-                category: category,
-                title: removeEscapeChar(title),
-                views: removeEscapeChar(views),
-                url: `${SMUOfficialBaseURL}${url}`
-            }
-        })
-        return todaysNoticeList;
-    }
-})
-.then((todaysNoticeList) => {
-    makeHTML(todaysNoticeList, month, date)
-    .then((html) => {
-        sendMail(html, formattedDate);
-        // console.log(html);
+            let todaysNoticeList = [];
+
+            $noticeList.each(function (idx, notice) {
+                const category = $(notice).find(categorySelector).text();
+                const title = $(notice).find(titleSelector).text();
+                const views = $(notice).find(viewsSelector).text();
+                const url = $(notice).find(titleSelector).attr("href");
+
+                todaysNoticeList[idx] = {
+                    category: category,
+                    title: removeEscapeChar(title),
+                    views: removeEscapeChar(views),
+                    url: `${SMUOfficialBaseURL}${url}`,
+                };
+            });
+            return todaysNoticeList;
+        }
+    })
+    .then((todaysNoticeList) => {
+        makeHTML(todaysNoticeList, month, date)
+            .then((html) => {
+                sendMail(html, formattedDate);
+            })
+            .catch((err) => {
+                throw new Error(err);
+            });
     })
     .catch((err) => {
-        return err;
-    })
-})
-.catch((err) => { console.log(err) })
+        throw new Error(err);
+    });
 
-// white space 제거하고 공백 요소 제거하기
+// white space 제거하고 공백 라인 제거하기
 const removeEscapeChar = (str) => {
-    let arr = str.split('\n');
+    let arr = str.split("\n");
 
-    let cleanedStr = arr.map((item) => {
-        return item.trim();
-    }).filter((item) => {
-        return item !== '';
-    })
+    let cleanedStr = arr
+        .map((item) => {
+            return item.trim();
+        })
+        .filter((item) => {
+            return item !== "";
+        });
 
     return cleanedStr;
-}
+};
--- utility/sendMail.js
+++ utility/sendMail.js
@@ -1,5 +1,5 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
 
 const { EMAIL_SERVICE, EMAIL_ADDRESS, EMAIL_PASSWORD } = process.env;
@@ -7,25 +7,25 @@ const { EMAIL_SERVICE, EMAIL_ADDRESS, EMAIL_PASSWORD } = process.env;
 const transporter = nodemailer.createTransport({
     service: EMAIL_SERVICE,
     auth: {
-        user: EMAIL_ADDRESS,    // 발신자 메일 주소
-        pass: EMAIL_PASSWORD    // 발신자 메일 비밀번호
-    }
+        user: EMAIL_ADDRESS, // 발신자 메일 주소
+        pass: EMAIL_PASSWORD, // 발신자 메일 비밀번호
+    },
 });
 
 export const sendMail = async (html, date) => {
     const mailOptions = {
-        from : EMAIL_ADDRESS,
+        from: EMAIL_ADDRESS,
         to: EMAIL_ADDRESS,
         subject: `[${date}] 상명대학교 공지사항 알림`,
-        html: html
+        html: html,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            return error
+            throw new Error(error);
         } else {
-            console.log('Email Sent : ', info);
+            console.log("Email Sent : ", info);
             return info;
         }
-    })
-}
+    });
+};
